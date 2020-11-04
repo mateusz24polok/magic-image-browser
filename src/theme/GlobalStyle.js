e4eb5421e72af65fc22db0b9c15f50588092fe19
Mateusz Polok <mateusz24polok@gmail.com>
2020-11-04 17:24:19 +0100

    Styled-components has been installed and GlobalStyle for whole app has been added

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap');
+
+    html{
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    *, ::after, ::before{
+        box-sizing: inherit
+    }
+`;
