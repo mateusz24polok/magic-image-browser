75dbc380a90ac1bde6bb1271d83d42148e343e3b
Mateusz Polok <mateusz24polok@gmail.com>
2020-11-06 19:03:27 +0100

    React Router has been implemented into app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./components/Main";
 import Form from "./components/Form";
 
 function App() {
   return (
-    <>
-      <Main>
-        <Form
-          title="Unsplash"
-          firstLineDescription="The internet's source of freely-usable-images."
-          secondLineDescription="Powered by creators everywhere"
-        />
-      </Main>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <>
+            <Main>
+              <Form
+                title="Unsplash"
+                firstLineDescription="The internet's source of freely-usable-images."
+                secondLineDescription="Powered by creators everywhere"
+              />
+            </Main>
+          </>
+        </Route>
+        <Route path="/photos">
+          <>
+            <div>Photos division</div>
+          </>
+        </Route>
+      </Switch>
+      <Route path="/photos/:id">
+        <>
+          <div>Photo division</div>
+        </>
+      </Route>
+    </BrowserRouter>
 
   );
 }
