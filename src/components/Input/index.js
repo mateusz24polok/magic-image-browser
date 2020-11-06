2b3f826d4ef768b0a4d05d3d2dad1081805a18a9
Mateusz Polok <mateusz24polok@gmail.com>
2020-11-06 22:38:54 +0100

    url path setting has been implemented into MainPage

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { StyledInput } from "./styled";
+import { selectQuery, setQuery } from "../../slices/querySlice";
 
 const Input = () => {
+    const dispatch = useDispatch();
+    const query = useSelector(selectQuery);
+
+    const onInputChange = (event) => {
+        dispatch(setQuery(event.target.value))
+    };
+
     return (
-        <StyledInput placeholder="example" />
+        <StyledInput onChange={onInputChange} value={query} placeholder="example" />
     );
 };
 
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import Main from "../../components/Main"
 import Form from "../../components/Form";
+import { selectQuery } from "../../slices/querySlice";
 
-const MainPage = () => (
-    <Main>
-        <Form
-            title="Unsplash"
-            firstLineDescription="The internet's source of freely-usable-images."
-            secondLineDescription="Powered by creators everywhere"
-        />
-    </Main>
-);
+const MainPage = () => {
+    const query = useSelector(selectQuery);
+    const history = useHistory();
+    const setUrlPhotoQuery = (query) => {
+        history.push(`photos?query=${query}`);
+    };
+
+    console.log(history);
+
+    return (
+        <Main>
+            <Form
+                onFormSubmit={() => setUrlPhotoQuery(query)}
+                title="Unsplash"
+                firstLineDescription="The internet's source of freely-usable-images."
+                secondLineDescription="Powered by creators everywhere"
+            />
+        </Main>
+    )
+};
 
 export default MainPage;
